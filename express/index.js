9697529bbff3e9a7f95e2d435665f32b403343f1
maciejmazurek <maciej.mazurek.prv@gmail.com>
2019-06-29 10:28:47 +0200

    express v1

--- express/index.js
+++ express/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const keys = require('./config/keys');
+
 const PORT = process.env.PORT || 5000;
-const usersRouter = require('./routes/api/users')
+const usersRouter = require('./routes/api/users');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,6 +17,11 @@ mongoose
   .connect(keys.mongoURI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
-
 app.use('/users', usersRouter);
-app.listen(PORT);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(`${__dirname  }/public/`));
+
+  app.get(/.*/, (req, res) => res.sendFile(`${__dirname  }/public/index.html`));
+}
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
